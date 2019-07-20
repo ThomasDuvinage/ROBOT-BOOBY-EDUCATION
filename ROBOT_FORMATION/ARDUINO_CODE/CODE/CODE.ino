428c5a7e9f156611e4455b8d90f8cd155435587e
totordudu <43036801+totordudu@users.noreply.github.com>
2019-07-20 10:58:44 +0200

    modify structure

---
+++ ROBOT_FORMATION/ARDUINO_CODE/CODE/CODE.ino
@@ -0,0 +1,33 @@
+#include <Stepper.h>
+ 
+// change this to fit the number of steps per revolution
+// nombre de step 
+const int stepsPerRevolution = 32*64;  
+
+const float diametre_roue = 52.3;//cette variable est a modifier en fonction de votre conception pour la roue 
+
+const float perimetre_roue = 3.14 * diametre_roue;// formule de 2*pi*r
+
+ 
+ 
+// initialize the stepper library on pins 8 through 11
+// initialisation de la librairie stepper
+// (step,1N2,1N4,1N3,1N1)
+Stepper droite(stepsPerRevolution, 11, 13, 12, 10
+);
+Stepper gauche(stepsPerRevolution, 5, 7, 6, 4);
+//Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+ 
+ 
+void setup() {
+ 
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  for(int i = 0; i<3 ; i++){
+    avancer(164.222,10, stepsPerRevolution, perimetre_roue);
+
+    tourner_droite(120,7,stepsPerRevolution,perimetre_roue);
+  }
+} 
---
+++ ROBOT_FORMATION/ARDUINO_CODE/CODE/MOTEUR.ino
@@ -0,0 +1,65 @@
+float distance_rotation(float angle){
+  const float rayon_robot = 56.45;// cette distance correspond à la ditance entre les joints toriques
+
+  float perimetre_angle = (angle * rayon_robot * 3.14) / 180;
+
+  return perimetre_angle;
+  
+}
+
+
+//cette fonction permet de caclculer le nombre de tour et ainsi le nombre de pas a faire pour parcourir une certaine distance
+int nombre_pas(float distance_parcourir, int steprevolution, float perimetre){
+  float tours = distance_parcourir / perimetre; // nous calculons le nombre de tours a faire
+  
+  int entier = floor(tours);//on extrait les entiers et les decimals afin de determiner le nombre de pas
+  float decimal = tours - entier;
+
+  int pas = (entier * steprevolution) + (decimal * steprevolution);// on calcul le nombre de pas
+
+  return pas ;
+}
+
+//on creer une fonction cette fonciton prend en parametre la distance a parcourir et la vitesse
+void avancer(float distance, int vitesse, int steprevolution, float perimetre){//vitesse comprise en 0 et 10
+  int pas = nombre_pas(distance,steprevolution,perimetre);// on recupere le nombre de pas 
+  
+  gauche.setSpeed(vitesse);
+  droite.setSpeed(vitesse);
+  
+  int n = 0;// on defini une variable qui sert de curseur 
+  for(n = 0; n<pas; n++){
+      gauche.step(1);
+      droite.step(-1);
+    }
+}
+
+//on creer une fonction cette fonciton prend en parametre la distance a parcourir et la vitesse
+void reculer(float distance, int vitesse, int steprevolution, float perimetre){//vitesse comprise en 0 et 10
+  int pas = nombre_pas(distance,steprevolution,perimetre);// on recupere le nombre de pas 
+  
+  gauche.setSpeed(vitesse);
+  droite.setSpeed(vitesse);
+  
+  int n = 0;// on defini une variable qui sert de curseur 
+  for(n = 0; n<pas; n++){
+      gauche.step(-1);
+      droite.step(1);
+    }
+}
+
+void tourner_droite(float angle, int vitesse, int steprevolution, float perimetre){
+  float distance = distance_rotation(angle);
+  int pas = nombre_pas(distance,steprevolution,perimetre);// on recupere le nombre de pas
+  Serial.print(pas);
+
+  gauche.setSpeed(vitesse);
+  droite.setSpeed(vitesse);
+  
+  int n = 0;// on defini une variable qui sert de curseur 
+  for(n = 0; n<pas; n++){
+      gauche.step(-1);
+      droite.step(-1);
+    }
+}
+
